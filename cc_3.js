cade1c87e21ff2e9cfcabd51cde3bd069e35d739
shanebrown77 <Bds1@usf.edu>
2025-09-10 17:55:50 -0400

    changed read me and some js

--- cc_3.js
+++ cc_3.js
@@ -1,14 +1,14 @@
-// in class coding challenge 
+// in class coding challenge 3
 
 let inventory = [
     {sku:"SKU-001",name:"Eco Bottle",price:19.99,stock:42},
      
-    {sku:"SKU-002",name:"Switch", price: 255,  stock:200},
+    {sku:"SKU-002",name:"Switch", price: 255,stock:200},
 
-    {sku:"SKU-003",name:"midi piano", price: 300 stock:50},
+    {sku:"SKU-003",name:"midi piano", price: 300,stock:50},
 
-    {sku:"SKU--004",name:"monster energy", price:2.99 stock:100},
+    {sku:"SKU--004",name:"monster energy", price:2.99,stock:100}
     
 ]
 
-inventory.forEach(obj =>console.log('Name: ${obj.name}'));
+inventory.forEach(obj =>console.log(`Name: ${obj.name}`));
